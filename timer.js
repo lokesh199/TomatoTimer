01cd75d1f5cb2388c7e2e8c40636bb309c352fa8
lokesh199 <lokesh.agarwal.1999@gmail.com>
2020-04-04 14:19:12 +0530

    added a blur effect

---
+++ timer.js
@@ -0,0 +1,7 @@
+var blurBackground = function (toggle) {
+    console.log("wow");
+    var background = document.getElementById("complete-page");
+    if (toggle == 1) {
+        background.style.filter = "blur(6px)";
+    }
+}
